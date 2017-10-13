c47f1c13cd7c0a1293e6b3db5cee756f110aeb04
姚明洋 <yaomy@go-goal.com>
2017-10-13 17:44:12 +0800

    优化控制器

--- src/main/java/ggauthority/v1/auth/ServletInitializer.java
+++ src/main/java/ggauthority/v1/auth/ServletInitializer.java
@@ -1,7 +1,7 @@
 package ggauthority.v1.auth;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
