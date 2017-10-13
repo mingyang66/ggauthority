b25907c7a1c446e978b34766ab427227fbf2229b
姚明洋 <yaomy@go-goal.com>
2017-10-13 15:36:51 +0800

    新建springboot项目

---
+++ src/main/java/ggauthority/v1/auth/GgauthorityApplication.java
@@ -0,0 +1,12 @@
+package ggauthority.v1.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgauthorityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgauthorityApplication.class, args);
+	}
+}
---
+++ src/main/java/ggauthority/v1/auth/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ggauthority.v1.auth;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GgauthorityApplication.class);
+	}
+
+}
